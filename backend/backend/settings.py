711049621ef4d7d61ff52be0520303fad9f27745
Tristan-Marcus <tristanjmarcus@gmail.com>
2021-07-09 02:09:10 -0700

    Added cors definitions in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'/diary/.*$'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
